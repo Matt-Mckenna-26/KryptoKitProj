3532db5a1170118e08020c1e0abd1234331d13ac
Daniel Henderson <djhindu17@gmail.com>
2021-03-05 12:54:29 -0600

    First Commit

--- client/src/components/BuySellForm.js
+++ client/src/components/BuySellForm.js
@@ -1,10 +1,20 @@
 import React, {useContext} from 'react';
+import axios from 'axios';
 
 const BuySellForm = () => {
 
     return(
         <div>
-
+            <h3>Choose Crypto</h3>
+            <h3>Funds</h3>
+            <form>
+                <input
+                    type="text"
+                    name="amountToSpend"
+                    placeholder="USD Currency (min $100)"
+                    // onChange={ (e) => setAmountToSpend(e.target.value)}
+                    />
+            </form>
         </div>
     )
 }
