a33551edaaacc33aede0f2e1552bc78b992b791a
Azrael Morherudaen <72979466+draconiis72@users.noreply.github.com>
2021-03-05 22:11:49 -0600

    initial login/reg setup

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import {Link} from '@reach/router';
+import axios from 'axios';
 
 
 const Nav = () => {
+    const logout = () => {
+        axios 
+          .post(
+            "http://localhost:8000/api/logout",
+            {},
+            {
+              withCredentials: true
+            }
+          )
+          .then(res => {
+            console.log(res);
+          })
+          .catch(console.log);
+      };
 
     return(
         <div>
             <h3>Main Navbar</h3>
+            <button onClick={logout} to="/">Logout</button>
             <Link to="/dashboard">Dashboard</Link>
             <Link to="/wallet">Crypto Wallet</Link>
             <Link to="/buysell">Buy/Sell</Link>
