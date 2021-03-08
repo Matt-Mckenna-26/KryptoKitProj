d62a7069c588a307116b4e4051eee963e19daaac
Daniel Henderson <djhindu17@gmail.com>
2021-03-07 22:55:43 -0600

    update

--- client/src/components/BuySellForm.js
+++ client/src/components/BuySellForm.js
@@ -16,10 +16,13 @@ import {
 
 import axios from 'axios';
 
-const BuySellForm = () => {
+const BuySellForm = (props) => {
     const [ userDollarsSpent, setUserDollarsSpent ] = useState("");
     const [ allCrypto, setAllCrypto ] = useState([]);
     const [ errs, setErrs ] = useState({});
+    const {loggedUser, focusCoin} = useContext(GlobalContext)
+    const [loggedInUser, setLoggedInUser] = loggedUser;
+    const [focusedCoin, setFocusedCoin] = focusCoin;
     
     useEffect(() => {
         axios
