b042b4d2ac7a606cf141cdb1648aa4481d69f9c6
Matt-Mckenna-26 <72038350+Matt-Mckenna-26@users.noreply.github.com>
2021-03-30 11:27:26 -0400

    small changes

--- server/server.js
+++ server/server.js
@@ -8,25 +8,8 @@ const cookieParser = require('cookie-parser');
 
 dotenv.config();
 
-const port = 8000 || process.env.PORT
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("../client/build"));
-	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-	});
-}
-
 const connection = 'database connection here'
 
-// This will fire our mongoose.connect statement to initialize our database connection
-mongoose.connect(process.env.MONGODB_URI || connection, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(() => console.log(`Established a connection to the  database`))
-	.catch(err => console.log("Something went wrong when connecting to the database", err));
 
 app.use(express.json(), express.urlencoded({ extended: true }));
 app.use(cors({credentials: true, origin:true}))
@@ -37,6 +20,24 @@ const AllMyUserRoutes = require("./routes/user.routes");
 const AllMyCoinRoutes = require("./routes/coin.routes")
 AllMyUserRoutes(app);
 AllMyCoinRoutes(app);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("../client/build"));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+	});
+}
+
+// This will fire our mongoose.connect statement to initialize our database connection
+mongoose.connect(process.env.MONGODB_URI || connection, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+    useFindAndModify: false
+})
+    .then(() => console.log(`Established a connection to the  database`))
+	.catch(err => console.log("Something went wrong when connecting to the database", err));
+
+const port = 8000 || process.env.PORT
 
 
 app.listen(port, () => console.log("The server is all fired up on " + port + ""));
